933cd4af2086fcf3225684f4fdf75fc3dd7e42c5
Matthew Wang <matwan@bergen.org>
2022-04-06 18:36:09 -0400

    day9_dom_practice

---
+++ day9_dom_practice.ts
@@ -0,0 +1,24 @@
+/* USING document.querySelector */
+
+// A: Make the title heading yellow
+
+// B : Make line 19 says "- Martin Fowler"
+
+// C: Make line 12 blue
+
+// D: Make line 2 red
+
+// E: Make line 8 monospace / spaced-out
+
+// F: Make line 11 green and uppercase - but ONLY after you click on it
+
+
+/* USING document.querySelectorAll */
+
+// G: Make lines 2, 5, 8, 12, 14, and 16 ALL bold
+
+// H: Make both lines 12 and 16 cursive and italic
+
+// I: Make ALL numbered lines respond to being clicked on by
+// *toggling* having a yellow background or not. 
+
